ce6bbaafba16e7114437fdc13db0e4ec46ec9db2
Ben Bi <49703109+ben4world@users.noreply.github.com>
2019-07-30 23:05:25 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,14 @@
+library(data.table)
+setwd("C:/Users/bib1/Documents/RStudio/MyProject/Module4Class1")
+fileurl = 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+if (!file.exists('./household_power_consumption.zip')){
+  download.file(fileurl,'./household_power_consumption.zip', mode = 'wb')
+  unzip("household_power_consumption.zip", exdir = getwd())
+}
+file<- read.table("household_power_consumption.txt",header=TRUE, sep=";", colClasses = c("character","character", rep("numeric",7)), na.strings = "?")
+file<-subset(file,file$Date=="1/2/2007" | file$Date == "2/2/2007")
+file$Date<-as.Date(file$Date, format="%d/%m/%Y")
+file$DateTime <- as.POSIXct(paste(file$Date, file$Time))
+
+hist(file$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency", main="Global Active Power")
+
---
+++ plot4.R
@@ -0,0 +1,27 @@
+library(datasets)
+library(data.table)
+setwd("C:/Users/bib1/Documents/RStudio/MyProject/Module4Class1")
+fileurl = 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+if (!file.exists('./household_power_consumption.zip')){
+  download.file(fileurl,'./household_power_consumption.zip', mode = 'wb')
+  unzip("household_power_consumption.zip", exdir = getwd())
+}
+file<- read.table("household_power_consumption.txt",header=TRUE, sep=";", colClasses = c("character","character", rep("numeric",7)), na.strings = "?")
+file<-subset(file,file$Date=="1/2/2007" | file$Date == "2/2/2007")
+file$Date<-as.Date(file$Date, format="%d/%m/%Y")
+file$DateTime <- as.POSIXct(paste(file$Date, file$Time))
+
+par(mfrow=c(2,2))
+with(file,{
+  plot(file$DateTime,file$Global_active_power,type="line",xlab="",ylab="Global Active Power")
+  
+  plot(file$DateTime,file$Voltage,type="line",xlab="datetime",ylab="Voltage")
+  
+  plot(file$DateTime,file$Sub_metering_1,type="n",xlab="", ylab="Energy sub metering")
+  points(file$DateTime,file$Sub_metering_1,type="line")  
+  points(file$DateTime,file$Sub_metering_2,type="line",col="red")
+  points(file$DateTime,file$Sub_metering_3,type="line",col="blue")
+  legend("topright", col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex = 0.8,lty = 1,bty = "n")
+  
+  plot(file$DateTime,file$Global_reactive_power,type="line",xlab="datetime",ylab="Global_reactive_power")
+})
